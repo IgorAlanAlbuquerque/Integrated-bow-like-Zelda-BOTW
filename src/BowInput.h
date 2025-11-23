f249b541d591347cea110cb54aef1397c404df93
igora <igoralbuquerque102@hotmail.com>
2025-11-23 15:16:47 -0300

    refactor(threads): :recycle: Definição da classe no .h e extraido threads
    Eu defini a classe que cuida dos inputs no .h para melhor organização e extraido o código de threads do onkeyrelesead para um helper para evitar duplicidade de código

--- src/BowInput.h
+++ src/BowInput.h
@@ -6,4 +6,27 @@ namespace BowInput {
     void SetHoldMode(bool hold);
     void SetKeyScanCodes(int k1, int k2, int k3);
     void SetGamepadButtons(int b1, int b2, int b3);
+
+    class IntegratedBowInputHandler : public RE::BSTEventSink<RE::InputEvent*> {
+    public:
+        static IntegratedBowInputHandler* GetSingleton();
+        RE::BSEventNotifyControl ProcessEvent(RE::InputEvent* const* a_events,
+                                              RE::BSTEventSource<RE::InputEvent*>*) override;
+
+    private:
+        static void ScheduleExitBowMode(bool waitBowDraw, int delayMs);
+        static void ScheduleAutoAttackDraw();
+        void UpdateHotkeyState(RE::PlayerCharacter* player, bool newKbdCombo, bool newPadCombo) const;
+        void HandleKeyboardButton(const RE::ButtonEvent* a_event, RE::PlayerCharacter* player) const;
+        void HandleGamepadButton(const RE::ButtonEvent* a_event, RE::PlayerCharacter* player) const;
+        void OnKeyPressed(RE::PlayerCharacter* player) const;
+        void OnKeyReleased() const;
+        static bool IsWeaponDrawn(RE::Actor* actor);
+        static void SetWeaponDrawn(RE::Actor* actor, bool drawn);
+        static RE::ExtraDataList* GetPrimaryExtra(RE::InventoryEntryData* entry);
+        static void EnterBowMode(RE::PlayerCharacter* player, RE::ActorEquipManager* equipMgr,
+                                 BowState::IntegratedBowState& st);
+        static void ExitBowMode(RE::PlayerCharacter* player, RE::ActorEquipManager* equipMgr,
+                                BowState::IntegratedBowState& st);
+    };
 }
