7cd64981f31ff47f43da93c51269e4c200b7a5f3
igora <igoralbuquerque102@hotmail.com>
2025-11-30 17:20:46 -0300

    fix: :bug: corrigido problema de limitar os botões do gamepad aos 30 primeiros números e adicionado função de auto detect

--- src/BowInput.h
+++ src/BowInput.h
@@ -7,6 +7,8 @@ namespace BowInput {
     void SetHoldMode(bool hold);
     void SetKeyScanCodes(int k1, int k2, int k3);
     void SetGamepadButtons(int b1, int b2, int b3);
+    void RequestGamepadCapture();
+    int PollCapturedGamepadButton();
 
     class IntegratedBowInputHandler : public RE::BSTEventSink<RE::InputEvent*> {
     public:
